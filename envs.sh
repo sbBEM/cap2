7e7021aadbb35d06dc9dac9124d194ec7288b3b4
thorr18 <bryceEmiller@gmail.com>
2021-04-14 10:14:15 -0700

    Format

--- envs.sh
+++ envs.sh
@@ -1,3 +1,3 @@
 conda env config vars set PROJ_LIB=~/opt/anaconda3/envs/SB/share/proj
 conda env config vars set GOOGLE_APPLICATION_CREDENTIALS="../../cap2-308607-82f97daf2ca4.json"
-conda env config vars set export ASDF="../../blah"
+export ASDF="../../blah"
