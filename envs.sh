d72781580469ab82793511660a9b435657e59be0
thorr18 <bryceEmiller@gmail.com>
2021-04-23 22:43:09 -0700

    add datafiles from NB1

--- envs.sh
+++ envs.sh
@@ -1,3 +1,3 @@
 conda env config vars set PROJ_LIB=~/opt/anaconda3/envs/SB/share/proj
-conda env config vars set GOOGLE_APPLICATION_CREDENTIALS="../../cap2-308607-82f97daf2ca4.json"
-export ASDF="../../blah"
+conda env config vars set GOOGLE_APPLICATION_CREDENTIALS="../../../cap2-308607-82f97daf2ca4.json"
+export old="../../old"
